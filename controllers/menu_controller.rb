fb5fdb68b2cdc903fbabcf8bdb7d30ccf3f77db8
Shaina Karasin <skarasin39@gmail.com>
2016-03-10 16:39:42 +0000

    assignment 20 Menu: completed

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -13,7 +13,8 @@ class MenuController
         puts "2 - Create an entry"
         puts "3 - Search for an entry"
         puts "4 - Import entries from a CSV"
-        puts "5 - Exit"
+        puts "5 - View entry by entry #"
+        puts "6 - Exit"
         print "Enter your selection: "
  
         selection = gets.to_i
@@ -36,6 +37,10 @@ class MenuController
             read_csv
             main_menu
         when 5
+            system "clear"
+            view_entry
+            main_menu
+        when 6
             puts "Good-bye!"
             exit(0)
         else
@@ -69,6 +74,45 @@ class MenuController
         
         system "clear"
         puts "New entry created successfully"
+    end
+    
+    def view_entry
+        if address_book.entries.count > 0
+            puts "Enter entry number: "
+        else
+            puts "No entries exist!"
+            main_menu
+        end
+        index = gets.to_i - 1
+        if (index >= 0) && (index < address_book.entries.count)
+            system "clear"
+            puts "Name: #{address_book.entries[index].name}"
+            puts "Phone Number: #{address_book.entries[index].phone_number}"
+            puts "Email Address: #{address_book.entries[index].email}"
+            view_entry_submenu
+        else
+            system "clear"
+            puts "Sorry, that is not a valid input, please try again"
+            view_entry
+        end
+    end
+    
+    def view_entry_submenu
+        puts "Enter 'v' to view another entry"
+        puts "or 'm'  to return to the main menu"
+        selection = gets.chomp
+        case selection
+        when "v"                
+            system "clear"
+            view_entry
+        when "m"
+            system "clear"
+            main_menu                
+        else #catches low numbers and letters
+            system "clear"
+            puts "Sorry, that is not a valid input, try again."
+            view_entry_submenu
+        end
     end
     
     def search_entries
